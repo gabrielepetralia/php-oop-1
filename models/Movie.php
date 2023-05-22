e8006f1bb189accf9f35f4d1e5fe9273bda92f8d
gabrielepetralia <gabri.petralia03@gmail.com>
2023-05-22 16:02:33 +0200

    create class Movie

--- models/Movie.php
+++ models/Movie.php
@@ -1,5 +1,20 @@
 <?php
 
 class Movie {
-  
+  public $poster;
+  public $title;
+  public $genre;
+  public $duration;
+
+  function __construct($_poster, $_title, $_genre, $_duration)
+  {
+    $this->poster = $_poster;
+    $this->title = $_title;
+    $this->genre = $_genre;
+    $this->duration = $_duration;
+  }
+
+  public function getDuration() {
+    return $this->duration . " min";
+  }
 }
